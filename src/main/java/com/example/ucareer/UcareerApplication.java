ad99bc49248ad2a67f2b0558d5af5d9b806c7dc3
Jiangqi Li <jiangqi319@gmail.com>
2020-02-04 21:10:41 -0500

    init

---
+++ src/main/java/com/example/ucareer/UcareerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ucareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcareerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcareerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ucareer/UcareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ucareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcareerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
